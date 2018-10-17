47ee518c97b8a1539c444660c7c87215d15e33b3
SinTod <pingxintao@gmail.com>
2018-10-17 18:04:23 +0800

    Application factory

---
+++ My_blog/extensions.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+'''
+ * @Author: SinTod
+ * @Date: 2018-10-17 17:41:37
+ * @Desc:
+'''
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+ckeditor = CKEditor()
+mail = Mail()
+moment = Moment()
